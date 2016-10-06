90112e620d7f3c1e9a39013ada557321e8a978ee
Óskar <Notandi@users.noreply.github.com>
2016-10-06 13:19:33 +0000

    Bætti við Character.js

---
+++ Character.js
@@ -0,0 +1,16 @@
+function Character(){
+	
+};
+
+Character.prototype.relations= [];
+Character.prototype.drama;
+Character.prototype.positivity;
+Character.prototype.unpredictability;
+Character.prototype.speak(){
+
+};
+Character.prototype.affect(){
+
+};
+
+
