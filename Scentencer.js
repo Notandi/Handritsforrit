8482ea068fa0a509fd9a308ddfceb5690bc76e1e
Óskar <Notandi@users.noreply.github.com>
2016-10-06 13:52:52 +0000

    bætti við Scentencer.js

---
+++ Scentencer.js
@@ -0,0 +1,21 @@
+function Scentencer(){
+	
+};
+
+Scentencer.prototype.nafnord = [];
+Scentencer.prototype.sagnord = [];
+Scentencer.prototype.lysingarord = [];
+Scentencer.prototype.spurnarfornofn = [];
+
+Scentencer.prototype.getText(){
+
+};
+Scentencer.prototype.enviromentDescription(){
+
+};
+Scentencer.prototype.eventDescription(){
+
+};
+Scentencer.prototype.reaction(){
+
+};
