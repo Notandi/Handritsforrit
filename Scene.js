d525e145531989c5172e4fbcd5ab42a4a06e4d1c
Óskar <Notandi@users.noreply.github.com>
2016-10-06 13:45:58 +0000

    bætti við Scene.js

---
+++ Scene.js
@@ -0,0 +1,21 @@
+function Scene(){
+	
+};
+
+Scene.prototype.characters = [];
+Scene.prototype.events = [];
+Scene.prototype.enviroment;
+Scene.prototype.text;
+Scene.prototype.sceneLength;
+Scene.prototype.eventPlacement;
+
+Scene.prototype.genrateSceneLength(){
+
+};
+Scene.prototype.genrateEventPlacement(){
+
+};
+
+Scene.prototype.reactToEvent(){
+
+};
